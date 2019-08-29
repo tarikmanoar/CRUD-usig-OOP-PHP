e5ad728b8144ac0afc2ff340ddbd2e9ab640d2e0
TarikManoar <tarikmanoar@gmail.com>
2019-08-29 19:37:33 +0600

    add Database

---
+++ oop.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2019 at 01:52 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine`
+--
+
+CREATE TABLE `medicine` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `medicine`
+--
+
+INSERT INTO `medicine` (`id`, `name`, `qty`) VALUES
+(1, 'Power Up', 120),
+(3, 'Cinamin', 500),
+(4, 'Parasitamal', 500),
+(5, 'Napa', 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(12) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `role` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `address`, `role`) VALUES
+(1, 'Tarik Mnaaor', 'samonta bazar', 'admin'),
+(2, 'TarikManoar', 'samonta', 'admin'),
+(3, 'manoar', 'moheshpur', 'admin'),
+(4, 'manoar', 'moheshpur', 'admin'),
+(5, 'manoar', 'moheshpur', 'admin'),
+(6, 'manoar', 'moheshpur', 'admin'),
+(7, 'manoar', 'moheshpur', 'admin'),
+(8, 'manoar', 'moheshpur', 'admin'),
+(9, 'manoar', 'moheshpur', 'admin'),
+(10, 'manoar', 'moheshpur', 'admin'),
+(11, 'manoar', 'moheshpur', 'admin'),
+(12, 'manoar', 'moheshpur', 'admin'),
+(13, 'manoar', 'moheshpur', 'admin'),
+(14, 'manoar', 'moheshpur', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `medicine`
+--
+ALTER TABLE `medicine`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `medicine`
+--
+ALTER TABLE `medicine`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
